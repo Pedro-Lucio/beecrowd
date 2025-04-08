86f06ac78159ac18ac6c8a40ae52f8d368b3d974
Pedro Lúcio Fernandes Garcia Leite <128326018+Pedro-Lucio@users.noreply.github.com>
2025-04-08 07:45:33 -0300

    Create  1002 - Área do Círculo.cpp

---
+++ 1002 - Área do Círculo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {	
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	
+	double r; cin >> r;
+	
+	printf("A=%.4f\n", 3.14159 * r * r);
+	
+	return 0;
+}
