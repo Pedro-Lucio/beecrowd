ec91ae3695f36adfa7060abac90bbab7b1125900
Pedro Lúcio Fernandes Garcia Leite <128326018+Pedro-Lucio@users.noreply.github.com>
2025-04-08 16:13:44 -0300

    Create  1113 - Crescente e Decrescente.cpp

---
+++ 1113 - Crescente e Decrescente.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    
+    while (true) {
+        cin >> x >> y;
+        if (x > y)
+            cout << "Decrescente" << endl;
+        else if (y > x)
+            cout << "Crescente" << endl;
+        else
+            break;
+    }
+
+    return 0;
+}
