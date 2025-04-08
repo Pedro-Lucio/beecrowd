308462e12209dc6d629fbbf14a89123e2c6cfe0c
Pedro Lúcio Fernandes Garcia Leite <128326018+Pedro-Lucio@users.noreply.github.com>
2025-04-08 07:48:06 -0300

    Create  1006 - Média 2.cpp

---
+++ 1006 - Média 2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {	
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	
+	float a, b, c; cin >> a >> b >> c;
+	
+	printf("MEDIA = %.1f\n", (a * 2 + b * 3 + c * 5) / (2 + 3 + 5));
+	
+	return 0;
+}
